b44a5aa1e7d1a509800c138bce7690119f6b16bd
Eduardo H Pollheim <49204024+edupollheim@users.noreply.github.com>
2025-01-29 16:22:37 -0300

    Analytics

---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,30 @@
+// components/GoogleAnalytics.js
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+  const GA_TRACKING_ID = 'G-6CN526BQ0Y'; // Substitua pelo seu ID de rastreamento
+
+  return (
+    <>
+      {/* Google tag (gtag.js) */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="google-analytics"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+        }}
+      />
+    </>
+  );
+};
+
+export default GoogleAnalytics;
